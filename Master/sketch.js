590c01357c9f578007f528d0c9d1fba0d94d83f7
WeiweiHung <112917097+WeiweiHung@users.noreply.github.com>
2024-12-02 22:58:48 +0800

    red Perlin Noise

--- Master/sketch.js
+++ Master/sketch.js
@@ -1,8 +1,25 @@
+let yoff = 0.0;
+
 function setup() {
-    createCanvas(400, 400);
-    background(220);
+  // A5 賀卡大小，300 DPI 對應像素值
+  createCanvas(1748, 2480); // 寬 1748 像素，高 2480 像素
+  background(0);//black
+  stroke(255, 0, 0); // 紅色曲線
+  strokeWeight(3);
+  noFill();
+}
+
+function draw() {
+  background(0, 0, 0, 20); // 加淡化效果
+
+  beginShape();
+  let xoff = 0;
+  for (let x = 0; x < width; x++) {
+    let y = map(noise(xoff, yoff), 0, 1, 200, 400);
+    vertex(x, y);
+    xoff += 0.05;
   }
-  
-  function draw() {
-    ellipse(mouseX, mouseY, 50, 50);
-  }
+  endShape();
+
+  yoff += 0.01;
+}
